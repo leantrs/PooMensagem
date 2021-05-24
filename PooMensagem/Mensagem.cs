8072e072cbaab88177af9f94d242f4b19ec142fd
marcel leandro <leantrus@hotmail.com>
2021-05-24 14:41:58 -0300

    Adicionar arquivos de projeto.

---
+++ PooMensagem/Mensagem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooMensagem
+{
+   public class Mensagem
+    {
+        /*
+       private String TextoMensagem;
+
+        public void ExibirMensagem() {
+            Console.WriteLine(this.TextoMensagem);
+        }
+
+        public String GetTextoMensagemm() {
+            return this.TextoMensagem;        
+        }
+
+        public void setTextoMensagem(String msg1)
+        {
+            this.TextoMensagem = msg1;
+        }
+        */
+
+        private String textoMensagem;
+
+        public String TextoMensagem
+        {
+            get  { return this.textoMensagem; }
+
+            set  { this.textoMensagem = value; }
+        }
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+
+
+
+    }
+}
---
+++ PooMensagem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1;
+            msg1 = new Mensagem();
+
+            msg1.TextoMensagem = "teste";
+            // msg1.ExibirMensagem();
+            Console.WriteLine(msg1.TextoMensagem);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
